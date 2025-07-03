3d8adb88939c916cfa80248fb8791fd7a4e096b6
Mamuhle <mamuhle07@gmail.com>
2025-07-03 16:08:07 +0200

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+
+my_dict ={'apple' :3, 'banana':5, 'orange':2}
+
+print(my_dict['apple'])
+
+my_dict['grapes'] = 4
+print(my_dict)
+
+my_dict['banana'] = 7
+print(my_dict)
+
+#Operators
+#Addition(+)
+#Subtraction(-)
+#Multiplication(*)
+#Division(/)
+#Modulus(%)
+#Exponent(**)
+
+x =10
+y =2
+x += 2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+
+print(x**y)
---
+++ lists.py
@@ -0,0 +1,18 @@
+
+fruits =["apple","banana","cherry"]
+
+print(fruits[0])
+
+fruits[1] ="blueberry"
+
+print(fruits)
+
+fruits =["apple","banana","cherry"]
+fruits.append("kiwi")
+print(fruits)
+
+fruits.insert(1,"orange")
+print(fruits)
+ 
+ 
+ 
